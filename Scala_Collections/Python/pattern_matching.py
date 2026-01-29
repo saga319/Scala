c813cd60ae1f87df965e596c8f5287d903cb168d
sagar.s1  <sagar.s1@subex.com>
2026-01-29 21:59:15 +0530

    changed end coats

--- Scala_Collections/Python/pattern_matching.py
+++ Scala_Collections/Python/pattern_matching.py
@@ -1,18 +1,18 @@
 n = 5
 for i in range(n + 1):
     for j in range(i):
-        print("*",end="")
+        print("*",end=" ")
     print()
 
 for i in range(n,0,-1):
     for j in range(i):
-        print("*",end="")
+        print("*",end=" ")
     print()
 
 for i in range(n,0,-1):
     for s in range(n - i):
-        print(" ",end="")
+        print(" ",end=" ")
     for j in range(i):
-        print("*",end="")
+        print("*",end=" ")
     print()
 
